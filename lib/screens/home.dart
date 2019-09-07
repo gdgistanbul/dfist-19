0af95ecd8f844f2b2fee09bb2afbbe2877082b97
Sercan Yusuf <srcnysf@gmail.com>
2019-09-07 15:27:14 +0300

    Added: constants, home and new theme

---
+++ lib/screens/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  PageController _pageController;
+  int _page = 0;
+s
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+
+  void navigationTapped(int page) {
+    _pageController.jumpToPage(page);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._page = page;
+    });
+  }
+}
