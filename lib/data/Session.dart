5d68216fe72f2870adb0541b92c8293b5038c4f6
mertceyhan <c.mertceyhan@hotmail.com>
2019-09-21 20:28:39 +0300

    Update for getting session detail

--- lib/data/Session.dart
+++ lib/data/Session.dart
@@ -1,6 +1,15 @@
 class Session {
+  String id;
   String title;
   String venue;
   String startTime;
   String endTime;
+
+  Session(id, title, venue, startTime, endTime) {
+    this.id = id;
+    this.title = title;
+    this.venue = venue;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
 }
--- lib/data/Speaker.dart
+++ lib/data/Speaker.dart
@@ -2,15 +2,30 @@ import 'package:dfist19/data/Session.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class Speaker {
-  String name;
-  String surname;
-  String bio;
-  String title;
-  String company;
-  String twitter;
-  Session session;
+  String id = "";
+  String name = "";
+  String surname = "";
+  String bio = "";
+  String title = "";
+  String company = "";
+  String twitter = "";
+  List<Session> sessions;
 
-  Speaker.fromDb(DataSnapshot data) {
+
+  Speaker.fromDataSnapshot(DataSnapshot data) {
+    id = data.key;
     name = data.value['name'];
+    surname = data.value['surname'];
+    bio = data.value['bio'];
+    title = data.value['title'];
+    company = data.value['company'];
+    twitter = data.value['twitter'];
+
+    sessions = new List();
+
+    Map<dynamic, dynamic> sessionsMap = data.value['sessions'];
+    sessionsMap.forEach((key, value) {
+      sessions.add(Session(key, value['title'], value['venue'], value['startTime'], value['endTime']));
+    });
   }
 }
