397a313f6cbf242909c7493a04f38354f2192ab6
mertceyhan <c.mertceyhan@hotmail.com>
2019-09-22 15:32:14 +0300

    Update for session detail

--- lib/screens/sessionDetail.dart
+++ lib/screens/sessionDetail.dart
@@ -1,33 +1,16 @@
-import 'package:dfist19/widgets/sessionItem.dart';
-import 'package:dfist19/widgets/socialMediaList.dart';
+import 'package:dfist19/data/Session.dart';
 import 'package:dfist19/widgets/speakerItem.dart';
 import 'package:flutter/material.dart';
-import 'package:dfist19/utils/sessions.dart';
-import 'package:dfist19/utils/const.dart';
 
 class SessionDetail extends StatefulWidget {
-  final String img;
-  final String name;
-  final String title;
-  final String description;
-  final String speaker;
-  final String time;
-  final String track;
 
-//  var session = new Session();
+  final Session session;
   final GestureTapCallback onPressed;
-  Map _sessions = sessions[0];
 
   SessionDetail({
     Key key,
-    @required this.img,
-    @required this.name,
-    @required this.title,
-    @required this.description,
+    @required this.session,
     @required this.onPressed,
-    @required this.speaker,
-    @required this.time,
-    @required this.track,
   }) : super(key: key);
 
   @override
@@ -59,16 +42,16 @@ class _SessionDetailState extends State<SessionDetail> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Padding(
-                            padding: const EdgeInsets.only(top:100.0),
+                              padding: const EdgeInsets.only(top: 100.0),
                             child: new Text(
-                                "Let's assume you have a product that is already working. This product has different problems or aspects that it has accomplished quite well.But at the end of the day, you needed to redesign your existing product because of the aesthetic concerns, or because of the new needs that arise over time. How will you manage this process?Should you change the current product in one go? Or is it better to spread this process over time? What will your users like in your new design? Is there really such an action needed? In this talk, let's learn how to manage a redesign process!Let's assume you have a product that is already working. This product has different problems or aspects that it has accomplished quite well.But at the end of the day, you needed to redesign your existing product because of the aesthetic concerns, or because of the new needs that arise over time. How will you manage this process?Should you change the current product in one go? Or is it better to spread this process over time? What will your users like in your new design? Is there really such an action needed? In this talk, let's learn how to manage a redesign process!Let's assume you have a product that is already working. This product has different problems or aspects that it has accomplished quite well.But at the end of the day, you needed to redesign your existing product because of the aesthetic concerns, or because of the new needs that arise over time. How will you manage this process?Should you change the current product in one go? Or is it better to spread this process over time? What will your users like in your new design? Is there really such an action needed? In this talk, let's learn how to manage a redesign process!Let's assume you have a product that is already working. This product has different problems or aspects that it has accomplished quite well.But at the end of the day, you needed to redesign your existing product because of the aesthetic concerns, or because of the new needs that arise over time. How will you manage this process?Should you change the current product in one go? Or is it better to spread this process over time? What will your users like in your new design? Is there really such an action needed? In this talk, let's learn how to manage a redesign process!",
+                                widget.session.description,
                                 style: TextStyle(
                                   fontFamily: 'RedHatDisplay',
                                   color: Color(0xff333d47),
                                   fontSize: 14,
                                   fontWeight: FontWeight.w400,
                                   fontStyle: FontStyle.normal,
-                                )),
+                                ))
                           ),
                           Padding(
                             padding: const EdgeInsets.only(top: 20.0),
@@ -115,7 +98,7 @@ class _SessionDetailState extends State<SessionDetail> {
                             width: 144.0,
                             child: Center(
                               child: SpeakerItem(
-                                name: widget.name,
+                                name: widget.session.speakerName,
                                 img: "assets/tickets.png",
                                 onPressed: () {},
                               ),
@@ -128,7 +111,6 @@ class _SessionDetailState extends State<SessionDetail> {
                 ),
               ),
             ),
-
             Container(
               child: Align(
                 alignment: Alignment.topCenter,
@@ -191,9 +173,10 @@ class _SessionDetailState extends State<SessionDetail> {
             Column(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.only(top: 80.0, left: 24.0, right: 24.0),
+                  padding:
+                  const EdgeInsets.only(top: 80.0, left: 24.0, right: 24.0),
                   child: new Text(
-                      "How to Understand Product Design Process as a Developer?",
+                      widget.session.title,
                       style: TextStyle(
                         fontFamily: 'RedHatDisplay',
                         color: Color(0xffffffff),
@@ -205,11 +188,12 @@ class _SessionDetailState extends State<SessionDetail> {
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 24.0, top: 16.0, bottom: 16.0),
+                    padding: const EdgeInsets.only(
+                        left: 24.0, top: 16.0, bottom: 16.0),
                     child: RichText(
                       text: new TextSpan(children: [
                         new TextSpan(
-                            text: widget.time,
+                            text: widget.session.startTime,
                             style: TextStyle(
                                 fontFamily: 'RedHatDisplay',
                                 color: Color(0xffffffff),
@@ -217,7 +201,7 @@ class _SessionDetailState extends State<SessionDetail> {
                                 fontWeight: FontWeight.w500,
                                 fontStyle: FontStyle.normal)),
                         new TextSpan(
-                            text: " //" + widget.track,
+                            text: " //" + widget.session.track,
                             style: TextStyle(
                                 fontFamily: 'RedHatDisplay',
                                 color: Color(0xffffffff),
